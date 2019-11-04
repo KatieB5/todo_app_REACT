e8ab89122d530b9c2f69ec77bf06e616946d0f91
KatieB5 <katie.bickford2@gmail.com>
2019-11-04 18:57:43 +0000

    Updates

---
+++ precache-manifest.bbeee943b0666f5dfb87000f77c1c394.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddbb23f85c964583f57bd09e12e0cb5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6353f190e5af02cef5b5",
+    "url": "/static/css/main.4a9e7131.chunk.css"
+  },
+  {
+    "revision": "c21ff3430102354fa616",
+    "url": "/static/js/2.db24b80f.chunk.js"
+  },
+  {
+    "revision": "6353f190e5af02cef5b5",
+    "url": "/static/js/main.9537078a.chunk.js"
+  },
+  {
+    "revision": "52ae44848fda7ea978e2",
+    "url": "/static/js/runtime-main.28b6cabb.js"
+  }
+]);
